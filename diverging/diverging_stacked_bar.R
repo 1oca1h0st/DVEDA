51fc63ac2b155b0e41bb299d8858944c95f4078d
loca1h0st <chenjh.network@gmail.com>
2020-03-15 14:49:45 +0800

    新增diverging stacked bar和csv数据

--- diverging/diverging_stacked_bar.R
+++ diverging/diverging_stacked_bar.R
@@ -1,3 +1,24 @@
 # https://rpubs.com/tskam/likert
 # https://luisdva.github.io/rstats/diverging-barplots/
 
+library(readr)
+library(dplyr)
+library(ggplot2)
+
+data = read_csv("diverging/diverging_stacked_bar_data.csv") %>%
+  mutate(contribution = round(contribution, 0))
+
+ggplot(data, aes(x = fct_rev((fct_inorder(
+  study
+))), y = contribution, fill = contributor)) +
+  geom_bar(stat = "identity",
+           position = "identity",
+           color = "dark grey") +
+  coord_flip() + ylim(-50, 200) +
+  geom_hline(yintercept = 0, color = c("white")) +
+  #theme_flat() +
+  xlab("study") + ylab("% contribution") +
+  ggtitle("Contributors to Global Warming over the past 50-65 years") +
+  scale_fill_manual(name = "", values = c("#FFA373", "#50486D")) +
+  labs(caption = "reproduced from skepticalscience.com") +
+  theme(text = element_text(family = "Roboto Medium"))
